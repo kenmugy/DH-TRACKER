9913f2caf49e471f91f73771c6c8a50ce2e09ef7
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-10-25 20:20:59 +0300

    Add login file

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+
+def login(request):
+    form = AuthenticationForm()
+    return render(request, "users/login", {"form": form})
+
