cf1a784f8ad11c9d16e6713aef302950072bf3f8
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-10-25 20:11:08 +0300

    Create Contact class for hold information for our contacts

--- track/models.py
+++ track/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    temperature = models.IntegerField(default=0)
+    company = models.CharField(max_length=150)
+    id_no = models.CharField(max_length=150, null=True)
+    telephone = models.CharField(max_length=150)
+    date = models.DateField(default=timezone.now)
+
