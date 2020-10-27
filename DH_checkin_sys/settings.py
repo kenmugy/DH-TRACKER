774fb73c3c152c7283fe50d83ef333f8dd254bdc
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-10-27 23:06:16 +0300

    Add login required decorators

--- DH_checkin_sys/settings.py
+++ DH_checkin_sys/settings.py
@@ -120,3 +120,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "home"
 
+LOGIN_URL = "login"
+
--- track/views.py
+++ track/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_list_or_404
 from django.forms import modelform_factory
 from django.contrib.messages import success
+from django.contrib.auth.decorators import login_required
 
 from .models import Contact
 from .forms import ContactForm
@@ -8,11 +9,13 @@ from .forms import ContactForm
 # ContactForm = modelform_factory(Contact, exclude=[])
 
 
+@login_required
 def home(request):
     contacts = get_list_or_404(Contact)
     return render(request, "track/home.html", {"contacts": contacts})
 
 
+@login_required
 def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
